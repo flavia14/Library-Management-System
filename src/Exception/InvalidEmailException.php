687446f532fa26429dbe83736d00e98855f55206
Flavia Andron <flavia.andron@pitech.webhelp.com>
2024-01-29 16:05:57 +0200

    Fix the class for pipeline.

--- src/Exception/InvalidEmailException.php
+++ src/Exception/InvalidEmailException.php
@@ -7,12 +7,17 @@ namespace App\Exception;
 use InvalidArgumentException;
 use Throwable;
 
+/**
+ * Exception thrown for invalid email address format.
+ */
 class InvalidEmailException extends InvalidArgumentException
 {
     /**
-     * @param string $message
-     * @param int $code
-     * @param Throwable|null $previous
+     * Constructor.
+     *
+     * @param string $message The exception message.
+     * @param int $code The exception code.
+     * @param Throwable|null $previous The previous throwable used for the exception chaining.
      */
     public function __construct(
         string $message = 'Invalid email address format',
