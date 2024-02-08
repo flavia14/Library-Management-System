b8062c03021e06973b8ab263e1460b2d3c8d27ff
Flavia Andron <flavia.andron@pitech.webhelp.com>
2024-02-08 17:40:57 +0200

    add observer pattern.

--- src/NotificationObserverVersion/NotificationService.php
+++ src/NotificationObserverVersion/NotificationService.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\NotificationObserverVersion;
 
-use App\Entity\SubjectCategory;
-
 class NotificationService implements SubjectInterface
 {
     /** @var array<SubjectObserverInterface> */
